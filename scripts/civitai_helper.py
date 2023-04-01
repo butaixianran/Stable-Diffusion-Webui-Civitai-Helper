0efcd03778d45ce568eaef00c7cc1dc8c3cdeace
Etokapa <31154606+Etokapa@users.noreply.github.com>
2023-04-01 00:33:00 -0500

    Update civitai_helper.py

--- scripts/civitai_helper.py
+++ scripts/civitai_helper.py
@@ -108,7 +108,7 @@ def on_ui_tabs():
                 gr.Markdown("### Scan Models for Civitai")
                 with gr.Row():
                     max_size_preview_ckb = gr.Checkbox(label="Download Max Size Preview", value=max_size_preview, elem_id="ch_max_size_preview_ckb")
-                    skip_nsfw_preview_ckb = gr.Checkbox(label="SKip NSFW Preview images", value=skip_nsfw_preview, elem_id="ch_skip_nsfw_preview_ckb")
+                    skip_nsfw_preview_ckb = gr.Checkbox(label="Skip NSFW Preview Images", value=skip_nsfw_preview, elem_id="ch_skip_nsfw_preview_ckb")
                     scan_model_types_ckbg = gr.CheckboxGroup(choices=model_types, label="Model Types", value=model_types)
 
                 # with gr.Row():
